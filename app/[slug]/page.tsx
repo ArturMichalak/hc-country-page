6235a186129f60658c16df7a1fb9a5aa93813d64
Artur Michalak <arturmichalak96@gmail.com>
2023-12-07 20:37:35 +0100

    tab title

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,8 +1,25 @@
-import { getCountries } from "@/app/page";
-import MainCard from "@/components/main-card";
-import Details from "@/fragments/details";
-import { Country, CountryDetails } from "@/helpers/sort";
-import _ from "lodash";
+import _ from 'lodash';
+import { Metadata } from 'next';
+
+import { getCountries } from '@/app/page';
+import MainCard from '@/components/main-card';
+import Details from '@/fragments/details';
+import { Country, CountryDetails } from '@/helpers/sort';
+
+export async function generateMetadata({params} : {params: any}): Promise<Metadata> {
+  const {slug} = params;
+  const countriesRaw = getCountries();
+  const [countries] = await Promise.all([countriesRaw]);
+
+  const country = countries.find(
+    (x) =>
+      _.kebabCase(x.name.common) === slug ||
+      _.kebabCase(x.name.official) === slug
+  ) as CountryDetails;
+  return {
+    title: `${country.name.common || country.name.official} - World Ranks`
+  }
+}
 
 export default async function Country({
   params,
